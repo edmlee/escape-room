5a39a4a081e33a19eebed454774852c07b63b8bf
Eddie Lee <112559345+edmlee@users.noreply.github.com>
2022-09-03 01:45:51 +1000

    Comments and Templates
    Added comments and created template for the selection numbers

--- escape-room.py
+++ escape-room.py
@@ -1,17 +1,28 @@
+# Track user's input
+def selection(start, end):
+    choice = input(f"Make your selection ({start}-{end}): ")
+    return choice
+
+# Menu screen for the 2 main actions
 def select_action(choice, room):
     print(f"1) Explore the {room}")
     print("2) Change Locations")
-    choice = input("Make your selection: ")
+    choice = selection(1, 2)
     return choice
 
+# Menu screen for the rooms
 def living_room(living_room_objects):
     for i in range(1, len(living_room_objects) + 1):
         print(f"{i}) {living_room_objects[i - 1]}")
+    choice = selection(1, len(living_room_objects))
+    return choice
 
+# Main
 rooms = ["Living Room", "Bedroom", "Bathroom", "Kitchen", "Garage"]
 living_room_objects = ["Couch", "Rug", "Plant", "TV", "Shelf"]
 inventory = []
 
+# Start Game
 choice = input("Do you want to play? (yes/no) ")
 
 if choice.lower().strip() == "yes":
@@ -20,8 +31,9 @@ if choice.lower().strip() == "yes":
 else:
     print("Maybe next time. Bye")
 
+# First task
 if choice.lower().strip() == "1":
-    living_room(living_room_objects)
+    choice = living_room(living_room_objects)
 else:
     print("next")
     
