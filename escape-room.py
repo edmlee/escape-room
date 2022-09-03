a906109826a8ff38edb19d00860976d90b262b44
Eddie Lee <112559345+edmlee@users.noreply.github.com>
2022-09-03 16:24:58 +1000

    Update escape-room.py
    Added locations function to find nearby rooms and print them in the terminal

--- escape-room.py
+++ escape-room.py
@@ -15,22 +15,37 @@ def selection(start, end):
     choice.end = end
     return choice
 
+
 # Menu screen for the 2 main actions
 def select_action(choice, current_room):
     print(f"You are in the {current_room}:")
-    print(f"1) Explore the room")
+    print("1) Explore the room")
     print("2) Change Locations")
     selection(1, 2)
     choice.current_room = current_room
     return choice
 
+
 # Menu screen for the rooms
-def living_room(living_room_objects):
-    choice.start, choice.end = 1, len(living_room_objects)
+def room(choice, room_objects):
+    choice.start = 1
+    choice.end = len(room_objects)
     for i in range(choice.start, choice.end + 1):
-        print(f"{i}) {living_room_objects[i - 1]}")
+        print(f"{i}) {room_objects[i - 1]}")
     selection(choice.start, choice.end)
     return choice
+
+
+def location(choice, rooms_list):
+    count = 1
+    choice.end = 2
+    nearby_room_index = [[1, 3], [0, 2], 1, [0, 4], 3]
+    nearby_rooms = {rooms_list[i]:j for i, j in enumerate(nearby_room_index)}
+    for r in nearby_rooms.get(choice.current_room):
+        print(f"{count}) Go to {rooms_list[r]}")
+        count += 1
+
+
 
 # Main
 rooms_list = ["Living Room", "Bedroom", "Bathroom", "Kitchen", "Garage"]
@@ -62,7 +77,10 @@ while(choice.answer != "quit" or quit_game != False):
     if game_started == True: 
         select_action(choice, current_room)
         if choice.answer == "1":
-            living_room(living_room_objects)
+            room(choice, living_room_objects)
+            quit()
+        elif choice.answer == "2":
+            location(choice, rooms_list)
             quit()
         else:
             if choice.answer != "quit":
