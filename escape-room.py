8b133e44c72a271fd3fc4adbaa903501e58c83d2
Eddie Lee <112559345+edmlee@users.noreply.github.com>
2022-09-03 15:17:18 +1000

    Update escape-room.py
    Updates

--- escape-room.py
+++ escape-room.py
@@ -1,39 +1,69 @@
+class Choice():
+    def __init__(self, answer, current_room, start, end):
+        self.answer = answer
+        self.current_room = current_room
+        self.start = start
+        self.end = end
+
+        answer = answer.lower().strip()
+
+
 # Track user's input
 def selection(start, end):
-    choice = input(f"Make your selection ({start}-{end}): ")
+    choice.answer = input(f"Make your selection ({start}-{end}): ")
+    choice.start = start
+    choice.end = end
     return choice
 
 # Menu screen for the 2 main actions
-def select_action(choice, room):
-    print(f"1) Explore the {room}")
+def select_action(choice, current_room):
+    print(f"You are in the {current_room}:")
+    print(f"1) Explore the room")
     print("2) Change Locations")
-    choice = selection(1, 2)
+    selection(1, 2)
+    choice.current_room = current_room
     return choice
 
 # Menu screen for the rooms
 def living_room(living_room_objects):
-    for i in range(1, len(living_room_objects) + 1):
+    choice.start, choice.end = 1, len(living_room_objects)
+    for i in range(choice.start, choice.end + 1):
         print(f"{i}) {living_room_objects[i - 1]}")
-    choice = selection(1, len(living_room_objects))
+    selection(choice.start, choice.end)
     return choice
 
 # Main
-rooms = ["Living Room", "Bedroom", "Bathroom", "Kitchen", "Garage"]
+rooms_list = ["Living Room", "Bedroom", "Bathroom", "Kitchen", "Garage"]
 living_room_objects = ["Couch", "Rug", "Plant", "TV", "Shelf"]
 inventory = []
+current_room = rooms_list[0]
+game_started = False
+quit_game = False
+game_over = False
 
 # Start Game
-choice = input("Do you want to play? (yes/no) ")
+choice = input("Do you want to play (yes/no)? ")
+choice = Choice(choice, current_room, 1, 2)
 
-if choice.lower().strip() == "yes":
-    print("You have been locked in an unknown house. You are located in the Living Room. Find the exit!")
-    choice = select_action(choice, rooms[0])
-else:
-    print("Maybe next time. Bye")
+while(choice.answer != "quit" or quit_game != False):    
+    # Start menu
+    if game_started == False:
+        if choice.answer == "yes":
+            print(f"You have been locked in an unknown house. Find the exit!")
+            game_started = True
+        elif choice.answer == "no":
+            print("Maybe next time. Bye")
+            quit()
+        else:
+            print("\nInvalid response. Please try again!")
+            choice.answer = input("Do you want to play (yes/no)? ")
 
-# First task
-if choice.lower().strip() == "1":
-    choice = living_room(living_room_objects)
-else:
-    print("next")
-    
+    # Start first task        
+    if game_started == True: 
+        select_action(choice, current_room)
+        if choice.answer == "1":
+            living_room(living_room_objects)
+            quit()
+        else:
+            if choice.answer != "quit":
+                print("\nInvalid response. Please try again!")
